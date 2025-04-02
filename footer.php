7d830d3554c5829002a751a8e804de4e2ef3c2bd
Rachel-Elble <Rachel.elble@gmail.com>
2025-04-01 20:22:19 -0400

    turned footer into a php file

---
+++ footer.php
@@ -0,0 +1,28 @@
+        <footer>
+            <div class="footer_links">
+                <h1><a href="#">CRAP PRINCIPLES</a></h1>  <!--Link to homepage-->
+                <p><a href="#">Contrast</a></p>
+                <p><a href="#">Repetition</a></p>
+                <p><a href="#">Allignment</a></p>
+                <p><a href="#">Proximity</a></p>
+                <div class="quiz_section">
+                    <div class="quiz_head">
+                        <p><a href="#">Quizzes</a></p>
+                        <button onclick="buttonToggle()" class="footer_arrow"><img src="assets/pictures/arrowDown_white.svg"></button>
+                    </div>
+                    <div id="q">
+                        <p><a href="#">Contrast Quiz</a></p>
+                        <p><a href="#">Repetition Quiz</a></p>
+                        <p><a href="#">Allignment Quiz</a></p>
+                        <p><a href="#">Proximity Quiz</a></p>
+                        <p><a href="#">Good vs Bad</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="footer_end">
+                <p>Copyright &copy; 2025</p>
+            </div>
+        </footer>
+    </body>
+    
+</html>
