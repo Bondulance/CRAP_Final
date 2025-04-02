5930e983928b3cd884c89d628642966f1aae46bd
Rachel-Elble <Rachel.elble@gmail.com>
2025-04-01 20:19:45 -0400

    navigation is PHP ready,

--- navigation.php
+++ navigation.php
@@ -6,7 +6,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/nav.css">
     <script src="./assets/js/nav.js" defer></script>
-    <title>Navigation</title>
+    <title><?php echo $pageTitle?></title>
 </head>
 <body>
     <header>
